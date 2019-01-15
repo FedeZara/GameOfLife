ee5bbdb27a48d1a1dad22ef53f7491eb3ea1e916
Lavoratorio <fedezara2001@gmail.com>
2019-01-15 08:42:31 +0100

    Multi threading development started

--- GameOfLife/Classes.cs
+++ GameOfLife/Classes.cs
@@ -5,13 +5,64 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace GameOfLife
 {
+    public class Griglia
+    {
+        public Random rnd { get; set; }
+        public CElemento[,] Elementi { get; set; }
+        public int Larghezza { get; set; }
+        public int Altezza { get; set; }
+        public Griglia(int larghezza, int altezza)
+        {
+            Elementi = new CElemento[larghezza, altezza];
+            Larghezza = larghezza;
+            Altezza = altezza;
+        }
+
+        public void Inizializza(int numConigli, int numLupi, int numCarote, int intervalloCarote)
+        {
+            int[,] Combinazioni = new int[Larghezza * Altezza, 2];
+            int[] indiciCombinazioni = new int[Larghezza * Altezza];
+            for (int x = 0; x < Larghezza; x++)
+            {
+                for (int y = 0; y < Altezza; y++)
+                {
+                    Combinazioni[y + x * Altezza, 0] = x;
+                    Combinazioni[y + x * Altezza, 1] = y;
+                    indiciCombinazioni[y + x * Altezza] = y + x * Altezza;
+                }
+            }
+            rnd = new Random();
+            indiciCombinazioni = indiciCombinazioni.OrderBy(x => rnd.Next()).ToArray();
+            int i = 0;
+            for (int j = 0; j < numConigli; j++)
+            {
+                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CConiglio(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
+                i++;
+            }
+            for (int j = 0; j < numLupi; j++)
+            {
+                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CLupo(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
+                i++;
+            }
+            for (int j = 0; j < numCarote; j++)
+            {
+                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CCarota(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
+                i++;
+            }
+        }
+    }
+
     public abstract class CElemento
     {
+        protected Griglia griglia;
+        private SemaphoreSlim mutex = new SemaphoreSlim(1, 1);
         protected PictureBox _PB;
         public bool DaEliminare { set; get; }
+        public CElemento(Griglia griglia) { this.griglia = griglia; }
         virtual public PictureBox PB
         {
             get
@@ -35,6 +86,14 @@ namespace GameOfLife
             this.X = X;
             this.Y = Y;
         }
+        public async Task OttieniAccesso()
+        {
+            await mutex.WaitAsync();
+        }
+        public void RilasciaAccesso()
+        {
+            mutex.Release();
+        }
     }
     public abstract class CAnimale : CElemento
     {
@@ -47,7 +106,9 @@ namespace GameOfLife
             }
         }
         public int Vita { get; set; }
-        public abstract void Muovi(CElemento[,] Elementi, bool[,] Occupato);
+        public CAnimale(Griglia griglia) : base(griglia) { }
+        
+        public abstract void Muovi();
         public void MostraPosizioneCambiata()
         {           
             PB.Location = new Point(X * FSimulazione.DimCasella, 60 + Y * FSimulazione.DimCasella);
@@ -68,114 +129,153 @@ namespace GameOfLife
             }
         }
 
-        public override void Muovi(CElemento[,] Elementi, bool[,] Occupato)
+        public CConiglio(Griglia griglia) : base(griglia) { }
+
+        public override async void Muovi()
         {
-            if(Vita == 0 || DaEliminare)
+            while (true)
             {
-                DaEliminare = true;
-                return;
-            }
-            bool NuovaPosizioneTrovata = false;
-            bool CarotaTrovata = false;
-            int newX = -1, newY = -1;
-            if (X > 0)
-            {
-                if (!Occupato[X - 1, Y])
+                if (Vita == 0 || DaEliminare)
                 {
-                    newX = X - 1;
+                    DaEliminare = true;
+                    return;
+                }
+                bool NuovaPosizioneTrovata = false;
+                bool CarotaTrovata = false;
+                int newX = -1, newY = -1;
+
+                await griglia.Elementi[X, Y].OttieniAccesso();
+
+                if (X > 0)
+                {
+                    await griglia.Elementi[X - 1, Y].OttieniAccesso();
+
+                    if (griglia.Elementi[X - 1, Y] == null)
+                    {
+                        newX = X - 1;
+                        newY = Y;
+                        NuovaPosizioneTrovata = true;
+                    }
+                    else if (griglia.Elementi[X - 1, Y] is CCarota){
+                        newX = X - 1;
+                        newY = Y;
+                        NuovaPosizioneTrovata = true;
+                        if (griglia.Elementi[X - 1, Y] is CCarota)
+                        {
+                            CarotaTrovata = true;
+                        }
+                    }
+
+                    if (!NuovaPosizioneTrovata)
+                        griglia.Elementi[X - 1, Y].RilasciaAccesso();
+                }
+
+                if (X < griglia.Elementi.GetLength(0) - 1)
+                {
+                    await griglia.Elementi[X + 1, Y].OttieniAccesso();
+
+                    if (CarotaTrovata && (griglia.Elementi[X + 1, Y] is CCarota) && griglia.rnd.Next(0, 100) < 50)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X + 1;
+                        newY = Y;
+                    }
+                    else if (!CarotaTrovata && (griglia.Elementi[X + 1, Y] is CCarota))
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X + 1;
+                        newY = Y;
+                        CarotaTrovata = true;
+                        NuovaPosizioneTrovata = true;
+                    }
+                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50) && griglia.Elementi[X + 1, Y] == null)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X + 1;
+                        newY = Y;
+                        NuovaPosizioneTrovata = true;
+                    }
+
+                    if(newX != X + 1 || newY != Y)
+                        griglia.Elementi[X + 1, Y].RilasciaAccesso();
+
+                }
+                if (Y > 0)
+                {
+                    await griglia.Elementi[X, Y - 1].OttieniAccesso();
+
+                    if (CarotaTrovata && (griglia.Elementi[X, Y - 1] is CCarota) && griglia.rnd.Next(0, 100) < 50)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y - 1;
+                    }
+                    else if (!CarotaTrovata && (griglia.Elementi[X, Y - 1] is CCarota))
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y - 1;
+                        CarotaTrovata = true;
+                        NuovaPosizioneTrovata = true;
+                    }
+                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50) && griglia.Elementi[X, Y - 1] == null)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y - 1;
+                        NuovaPosizioneTrovata = true;
+                    }
+
+                    if (newX != X || newY != Y - 1)
+                        griglia.Elementi[X, Y - 1].RilasciaAccesso();
+                }
+                if (Y < griglia.Elementi.GetLength(1) - 1)
+                {
+                    await griglia.Elementi[X, Y - 1].OttieniAccesso();
+                    if (CarotaTrovata && (griglia.Elementi[X, Y + 1] is CCarota) && griglia.rnd.Next(0, 100) < 50)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y + 1;
+                    }
+                    else if (!CarotaTrovata && (griglia.Elementi[X, Y + 1] is CCarota))
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y + 1;
+                        CarotaTrovata = true;
+                        NuovaPosizioneTrovata = true;
+                    }
+                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50) && griglia.Elementi[X, Y - 1] == null)
+                    {
+                        griglia.Elementi[newX, newY].RilasciaAccesso();
+                        newX = X;
+                        newY = Y + 1;
+                        NuovaPosizioneTrovata = true;
+                    }
+
+                    if (newX != X || newY != Y + 1)
+                        griglia.Elementi[X, Y + 1].RilasciaAccesso();
+                }
+                if (CarotaTrovata)
+                {
+                    Vita = 11;
+                    griglia.Elementi[newX, newY].DaEliminare = true;
+                }
+                Vita--;
+                if (newX == -1)
+                {
+                    newX = X;
                     newY = Y;
-                    NuovaPosizioneTrovata = true;
-                    if (Elementi[X - 1, Y] is CCarota)
-                    {
-                        CarotaTrovata = true;
-                    }
                 }
-            }
-            if (X < Elementi.GetLength(0) - 1)
-            {
-                if (!Occupato[X + 1, Y])
-                {
-                    if (CarotaTrovata && (Elementi[X + 1, Y] is CCarota) && FSimulazione.rnd.Next(0, 100) < 50)
-                    {
-                        newX = X + 1;
-                        newY = Y;
-                    }
-                    else if (!CarotaTrovata && (Elementi[X + 1, Y] is CCarota))
-                    {
-                        newX = X + 1;
-                        newY = Y;
-                        CarotaTrovata = true;
-                        NuovaPosizioneTrovata = true;
-                    }
-                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
-                    {
-                        newX = X + 1;
-                        newY = Y;
-                        NuovaPosizioneTrovata = true;
-                    }
-                }
-            }
-            if (Y > 0)
-            {
-                if (!Occupato[X, Y - 1])
-                {
-                    if (CarotaTrovata && (Elementi[X, Y - 1] is CCarota) && FSimulazione.rnd.Next(0, 100) < 50)
-                    {
-                        newX = X;
-                        newY = Y - 1;
-                    }
-                    else if (!CarotaTrovata && (Elementi[X, Y - 1] is CCarota))
-                    {
-                        newX = X;
-                        newY = Y - 1;
-                        CarotaTrovata = true;
-                        NuovaPosizioneTrovata = true;
-                    }
-                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
-                    {
-                        newX = X;
-                        newY = Y - 1;
-                        NuovaPosizioneTrovata = true;
-                    }
-                }
-            }
-            if (Y < Elementi.GetLength(1) - 1)
-            {
-                if (!Occupato[X, Y + 1])
-                {
-                    if (CarotaTrovata && (Elementi[X, Y + 1] is CCarota) && FSimulazione.rnd.Next(0, 100) < 50)
-                    {
-                        newX = X;
-                        newY = Y + 1;
-                    }
-                    else if (!CarotaTrovata && (Elementi[X, Y + 1] is CCarota))
-                    {
-                        newX = X;
-                        newY = Y + 1;
-                        CarotaTrovata = true;
-                        NuovaPosizioneTrovata = true;
-                    }
-                    else if (!CarotaTrovata && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
-                    {
-                        newX = X;
-                        newY = Y + 1;
-                        NuovaPosizioneTrovata = true;
-                    }
-                }
-            }
-            if (CarotaTrovata)
-            {
-                Vita = 11;
-                Elementi[newX, newY].DaEliminare = true;
-            }
-            Vita--;
-            if(newX == -1) {
-                newX = X;
-                newY = Y;
-            }
-            Occupato[newX, newY] = true;
-            X = newX;
-            Y = newY;
+
+                X = newX;
+                Y = newY;
+
+                griglia.Elementi[X, Y].RilasciaAccesso();
+
+                await Task.Delay(griglia.rnd.Next(500, 3000));
+            }            
         }
         public CConiglio(int X, int Y) : base(X, Y)
         {
@@ -192,7 +292,8 @@ namespace GameOfLife
                 return GameOfLife.Properties.Resources.lupo;
             }
         }
-        public override void Muovi(CElemento[,] Elementi, bool[,] Occupato)
+        public CLupo(Griglia griglia) : base(griglia) { }
+        public override void Muovi(CElemento[,] griglia.Elementi, bool[,] Occupato)
         {
             if (Vita == 0 || DaEliminare)
             {
@@ -206,35 +307,35 @@ namespace GameOfLife
             {
                 if(!Occupato[X-1, Y])
                 {
-                    if (!(Elementi[X - 1, Y] is CCarota))
+                    if (!(griglia.Elementi[X - 1, Y] is CCarota))
                     {
                         NuovaPosizioneTrovata = true;
                         newX = X - 1;
                         newY = Y;
-                        if (Elementi[X - 1, Y] is CConiglio)
+                        if (griglia.Elementi[X - 1, Y] is CConiglio)
                         {
                             ConiglioTrovato = true;
                         }
                     }
                 }
             }
-            if(X < Elementi.GetLength(0) - 1)
+            if(X < griglia.Elementi.GetLength(0) - 1)
             {
                 if (!Occupato[X + 1, Y])
                 {
-                    if(ConiglioTrovato && (Elementi[X + 1, Y] is CConiglio) && FSimulazione.rnd.Next(0, 100) < 50)
+                    if(ConiglioTrovato && (griglia.Elementi[X + 1, Y] is CConiglio) && griglia.rnd.Next(0, 100) < 50)
                     {
                         newX = X + 1;
                         newY = Y;
                     }
-                    else if(!ConiglioTrovato && (Elementi[X + 1, Y] is CConiglio))
+                    else if(!ConiglioTrovato && (griglia.Elementi[X + 1, Y] is CConiglio))
                     {
                         newX = X + 1;
                         newY = Y;
                         ConiglioTrovato = true;
                         NuovaPosizioneTrovata = true;
                     }
-                    else if (!ConiglioTrovato && !(Elementi[X + 1, Y] is CCarota) && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
+                    else if (!ConiglioTrovato && !(griglia.Elementi[X + 1, Y] is CCarota) && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50))
                     {
                         newX = X + 1;
                         newY = Y;
@@ -246,19 +347,19 @@ namespace GameOfLife
             {
                 if (!Occupato[X, Y-1])
                 {
-                    if (ConiglioTrovato && (Elementi[X, Y - 1] is CConiglio) && FSimulazione.rnd.Next(0, 100) < 50)
+                    if (ConiglioTrovato && (griglia.Elementi[X, Y - 1] is CConiglio) && griglia.rnd.Next(0, 100) < 50)
                     {
                         newX = X;
                         newY = Y-1;
                     }
-                    else if (!ConiglioTrovato && (Elementi[X, Y - 1] is CConiglio))
+                    else if (!ConiglioTrovato && (griglia.Elementi[X, Y - 1] is CConiglio))
                     {
                         newX = X;
                         newY = Y - 1;
                         ConiglioTrovato = true;
                         NuovaPosizioneTrovata = true;
                     }
-                    else if (!ConiglioTrovato && !(Elementi[X, Y-1] is CCarota) && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
+                    else if (!ConiglioTrovato && !(griglia.Elementi[X, Y-1] is CCarota) && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50))
                     {
                         newX = X;
                         newY = Y - 1;
@@ -266,23 +367,23 @@ namespace GameOfLife
                     }
                 }
             }
-            if (Y < Elementi.GetLength(1) - 1)
+            if (Y < griglia.Elementi.GetLength(1) - 1)
             {
                 if (!Occupato[X, Y + 1])
                 {
-                    if (ConiglioTrovato && (Elementi[X, Y + 1] is CConiglio) && FSimulazione.rnd.Next(0, 100) < 50)
+                    if (ConiglioTrovato && (griglia.Elementi[X, Y + 1] is CConiglio) && griglia.rnd.Next(0, 100) < 50)
                     {
                         newX = X;
                         newY = Y + 1;
                     }
-                    else if (!ConiglioTrovato && (Elementi[X, Y + 1] is CConiglio))
+                    else if (!ConiglioTrovato && (griglia.Elementi[X, Y + 1] is CConiglio))
                     {
                         newX = X;
                         newY = Y + 1;
                         ConiglioTrovato = true;
                         NuovaPosizioneTrovata = true;
                     }
-                    else if (!ConiglioTrovato && !(Elementi[X, Y+1] is CCarota) && (!NuovaPosizioneTrovata || FSimulazione.rnd.Next(0, 100) < 50))
+                    else if (!ConiglioTrovato && !(griglia.Elementi[X, Y+1] is CCarota) && (!NuovaPosizioneTrovata || griglia.rnd.Next(0, 100) < 50))
                     {
                         newX = X;
                         newY = Y + 1;
@@ -294,7 +395,7 @@ namespace GameOfLife
             {
                 Vita = 11;
 
-                Elementi[newX, newY].DaEliminare = true;
+                griglia.Elementi[newX, newY].DaEliminare = true;
             }
             Vita--;
             if (newX == -1)
@@ -314,6 +415,7 @@ namespace GameOfLife
 
     public class CCarota : CElemento
     {
+        public CCarota(Griglia griglia) : base(griglia) { }
         public override Image Immagine
         {
             get
--- GameOfLife/FSimulazione.cs
+++ GameOfLife/FSimulazione.cs
@@ -14,11 +14,9 @@ namespace GameOfLife
 { 
 
     public partial class FSimulazione : Form
-    {
-        public CElemento[,] Elementi { get; set; }
-        
-        public static int DimCasella = 30;
-        public static Random rnd { get; set; }
+    {         
+        public Griglia Griglia { get; set; }
+        public const int DimCasella = 30;
         private int TimerCarota;
         private int IntervalloCarote;
         private Task CalcoloPosizioniTask;
@@ -28,54 +26,23 @@ namespace GameOfLife
         {
             InitializeComponent();
 
-            Elementi = new CElemento[wGriglia,hGriglia];
+            Griglia = new Griglia(wGriglia, hGriglia);
 
-            Inizializza(wGriglia, hGriglia, numConigli, numLupi, numCarote, intervalloCarote);
-        }
+            Griglia.Inizializza(numConigli, numLupi, numCarote, intervalloCarote);
 
-        public void Inizializza(int wGriglia, int hGriglia, int numConigli, int numLupi, int numCarote, int intervalloCarote)
-        {
             TimerCarota = 0;
             IntervalloCarote = intervalloCarote;
             this.AutoSize = true;
             this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            int[,] Combinazioni = new int[wGriglia * hGriglia, 2];
-            int[] indiciCombinazioni = new int[wGriglia * hGriglia];
-            for(int x=0; x<wGriglia; x++)
-            {
-                for(int y=0; y<hGriglia; y++)
-                {
-                    Combinazioni[y + x * hGriglia, 0] = x;
-                    Combinazioni[y + x * hGriglia, 1] = y;
-                    indiciCombinazioni[y + x * hGriglia] = y + x * hGriglia;
-                }
-            }
-            rnd = new Random();
-            indiciCombinazioni = indiciCombinazioni.OrderBy(x => rnd.Next()).ToArray();
-            int i = 0;
-            for(int j=0; j<numConigli; j++)
-            {
-                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CConiglio(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
-                i++;
-            }
-            for (int j = 0; j < numLupi; j++)
-            {
-                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CLupo(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
-                i++;
-            }
-            for (int j = 0; j < numCarote; j++)
-            {
-                Elementi[Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]] = new CCarota(Combinazioni[indiciCombinazioni[i], 0], Combinazioni[indiciCombinazioni[i], 1]);
-                i++;
-            }
 
-            for (int x = 0; x < Elementi.GetLength(0); x++)
+
+            for (int x = 0; x < Griglia.Elementi.GetLength(0); x++)
             {
-                for (int y = 0; y < Elementi.GetLength(1); y++)
+                for (int y = 0; y < Griglia.Elementi.GetLength(1); y++)
                 {
-                    if (!(Elementi[x, y] is null))
+                    if (!(Griglia.Elementi[x, y] is null))
                     {
-                        this.Controls.Add(Elementi[x, y].PB);
+                        this.Controls.Add(Griglia.Elementi[x, y].PB);
                     }
                 }
             }
@@ -88,10 +55,7 @@ namespace GameOfLife
             Impostazioni.Location = new Point(0, 0);
             Pannello.Controls.Add(Impostazioni);
             this.Controls.Add(Pannello);
-
-            CalcoloPosizioniTask = Task.Run(() => CalcoloPosizioni());
         }
-
 
         public void Risultati(int ConigliRimasti, int LupiRimasti)
         {
@@ -99,22 +63,6 @@ namespace GameOfLife
             this.Hide();
             Risultati.ShowDialog();
             this.Close();
-        }
-
-        public void CalcoloPosizioni()
-        {
-            //Calcola una nuova posizione per ogni animale presente nella griglia
-            bool[,] Occupato = new bool[Elementi.GetLength(0), Elementi.GetLength(1)];
-            for (int x = 0; x<Elementi.GetLength(0); x++)
-            {
-                for (int y = 0; y<Elementi.GetLength(1); y++)
-                {
-                    if (Elementi[x, y] is CAnimale)
-                    {
-                        (Elementi[x, y] as CAnimale).Muovi(Elementi, Occupato);                
-                    }
-                }
-            }
         }
 
         public void AggiornaPosizioni()
